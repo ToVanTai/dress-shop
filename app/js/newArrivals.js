996332f46917aa113c13dc7daa5a02334a7d75b4
tovantai <tovantaidz2001@gmail.com>
2021-12-13 19:25:17 +0700

    update dbjson

--- app/js/newArrivals.js
+++ app/js/newArrivals.js
@@ -6,7 +6,6 @@ const swiperPromise = new Promise((resolve, reject) => {
 });
 swiperPromise.then((data) => {
     let dataParse = JSON.parse(data.response);
-    console.log(dataParse);
     let mySwiperWrapper = conFig.$(".my-swiper-wrapper");
     for (let i = 0; i < dataParse.length; i++) {
         mySwiperWrapper.innerHTML += `<div class="swiper-slide">
